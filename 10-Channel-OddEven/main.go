6de8a4a31b0d0d842bfcf57a33ac5446bda8fb97
akashp <akashp@mkcl.org>
2025-12-05 14:21:41 +0530

    odd even using wg

--- 10-Channel-OddEven/main.go
+++ 10-Channel-OddEven/main.go
@@ -2,32 +2,35 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
-func odd(ch chan int) {
-	for i := 1; ; i += 2 {
-		ch <- i
-	}
-}
-
-func even(ch chan int) {
-	for i := 2; ; i += 2 {
-		ch <- i
-	}
-}
-
-func main() {
-	oddChan := make(chan int)
-	evenChan := make(chan int)
-
-	go odd(oddChan)
-	go even(evenChan)
-
-	for i := 1; i <= 10; i++ {
-		if i%2 != 0 {
-			fmt.Println(<-oddChan)
-		} else {
-			fmt.Println(<-evenChan)
+func printEven(oddChan, evenChan chan bool, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i := 0; i <= 10; i = i + 2 {
+		<-evenChan
+		fmt.Println(i)
+		if i < 10 {
+			oddChan <- true
 		}
 	}
+}
+func printOdd(oddChan, evenChan chan bool, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i := 1; i <= 10; i = i + 2 {
+		<-oddChan
+		fmt.Println(i)
+		evenChan <- true
+	}
+}
+func main() {
+	var wg sync.WaitGroup
+	oddChan := make(chan bool)
+	evenChan := make(chan bool)
+	wg.Add(2)
+	go printOdd(oddChan, evenChan, &wg)
+	go printEven(oddChan, evenChan, &wg)
+	evenChan <- true
+	wg.Wait()
+	fmt.Println("Done")
 }
